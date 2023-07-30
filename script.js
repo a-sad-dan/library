48fb59d02623cca7808f788ad874d5898dc811b0
Dan <danishasad91@gmail.com>
2023-07-30 21:10:50 +0530

    added form toggle functionality

--- script.js
+++ script.js
@@ -10,8 +10,34 @@ function Book(title, author, pages, isRead) {
 }
 
 //To add books to the library
-function addBookToLibrary()
-{
+function addBookToLibrary() {
     const myBook = new Book();
     myLibrary.push(myBook)
 }
+
+
+
+
+
+
+
+
+
+// Light/Dark Mode toggle
+document.getElementById('toggle-btn').addEventListener('click', () => {
+    document.documentElement.classList.toggle('invert');
+    document.querySelector('.header').classList.toggle('no-shadow');
+    document.querySelector('.sidebar').classList.toggle('no-shadow');
+
+})
+
+
+
+bookBtn = document.querySelector('#add-book');
+bookBtn.addEventListener('click', () => {
+
+    bookBtn.classList.toggle('rotated');
+    document.querySelector('.user-stats').classList.toggle('hidden');
+    document.querySelector('.book-form').classList.toggle('hidden');
+
+})
