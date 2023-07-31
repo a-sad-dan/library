4448274a01958ff9e5ef8a4995f80db04f805827
Dan <danishasad91@gmail.com>
2023-07-31 16:01:46 +0530

    Form now adds books to MyLibrary

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Storing All the book objects in an array
-let myLibrary = [];
+let myLibrary = [1, 2, 23, 4, 4,2, 23, 4, 4];
 
 // To Create Book objects
 function Book(title, author, pages, isRead) {
@@ -11,14 +11,42 @@ function Book(title, author, pages, isRead) {
 
 //To add books to the library
 function addBookToLibrary() {
-    const myBook = new Book();
-    myLibrary.push(myBook)
+    const authorName = document.getElementById('author-name').value;
+    const bookName = document.getElementById('book-name').value;
+    const numPages = document.getElementById('num-pages').value;
+    const isRead = document.getElementById('read').checked;
+    const myBook = new Book(bookName, authorName, numPages, isRead);
+    myLibrary.push(myBook);
+    console.table(myLibrary);
+    document.querySelector('#book-form').reset()
 }
 
+const mainArea = document.querySelector('.main');
+// Function to display the Book Cards
+function updateCard() {
+    console.log('started updating cards');
+    myLibrary.forEach(element => {
+        const newCard = document.createElement('div');
+        const cardHtml = `
+    
+      <img class="book-cover" src="card/book-cover.png" alt="">
+      <div class="sleeve">
+          <img class="cross" id ="remove-book" src="card/cross.svg" alt="">
+          <p class="book-title">Harry Potter and The Goblet of Fire</p>
+          <p class="book-author">J.K. Rowling</p>
+          <p class="pages-info">238</p>
+          <div class="icons">
+              <img src="card/heart.svg" alt="" class="card-icon">
+              <img src="card/check-square.svg" alt="" class="card-icon">
+          </div>
+      </div>
 
-
-
-
+  `;
+        newCard.innerHTML = cardHtml;
+        newCard.classList.add('card');
+        mainArea.appendChild(newCard);
+    });
+}
 
 
 
@@ -41,3 +69,6 @@ bookBtn.addEventListener('click', () => {
     document.querySelector('.book-form').classList.toggle('hidden');
 
 })
+
+
+updateCard();
