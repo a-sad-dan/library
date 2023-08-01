31d4f604f1e38850b70a578745728d1975e1a14e
Dan <danishasad91@gmail.com>
2023-08-01 14:49:06 +0530

    Added book render function

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 // Storing All the book objects in an array
-let myLibrary = [1, 2, 23, 4, 4,2, 23, 4, 4];
+let myLibrary = [
+    {
+        "title": "Harry Potter and the Philosphers Stone",
+        "author": "J.K. Rowling",
+        "pages": "300",
+        "isRead": true,
+        "imgLink": "https://c8.alamy.com/comp/HMHD9T/harry-potter-and-the-philosophers-stone-HMHD9T.jpg"
+    },
+    {
+        "title": "Harry Potter and the Deathly Hallows",
+        "author": "Danish Asad",
+        "pages": "213",
+        "isRead": false,
+        "imgLink": "https://www.nicepng.com/png/full/895-8959915_please-note-jk-rowling-harry-potter-book-cover.png"
+    },
+    {
+        "title": "Harry Potter and the Chamber of secrets",
+        "author": "j.k. rowling",
+        "pages": "450",
+        "isRead": false,
+        "imgLink": "https://wallpaper.dog/large/198914.jpg"
+    }
+];
 
 // To Create Book objects
-function Book(title, author, pages, isRead) {
+function Book(title, author, pages, isRead, imgLink) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
+    this.imgLink = imgLink;
 }
 
 //To add books to the library
@@ -14,11 +37,12 @@ function addBookToLibrary() {
     const authorName = document.getElementById('author-name').value;
     const bookName = document.getElementById('book-name').value;
     const numPages = document.getElementById('num-pages').value;
+    const imgLink = document.getElementById('img-url').value;
     const isRead = document.getElementById('read').checked;
-    const myBook = new Book(bookName, authorName, numPages, isRead);
+    const myBook = new Book(bookName, authorName, numPages, isRead, imgLink);
     myLibrary.push(myBook);
     console.table(myLibrary);
-    document.querySelector('#book-form').reset()
+    document.querySelector('#book-form').reset();
 }
 
 const mainArea = document.querySelector('.main');
@@ -27,27 +51,28 @@ function updateCard() {
     console.log('started updating cards');
     myLibrary.forEach(element => {
         const newCard = document.createElement('div');
-        const cardHtml = `
-    
-      <img class="book-cover" src="card/book-cover.png" alt="">
+        const cardHtml = `<img class="book-cover" src=${element.imgLink} alt="Book-cover">
       <div class="sleeve">
-          <img class="cross" id ="remove-book" src="card/cross.svg" alt="">
-          <p class="book-title">Harry Potter and The Goblet of Fire</p>
-          <p class="book-author">J.K. Rowling</p>
-          <p class="pages-info">238</p>
+          <img class="cross" id ="remove-book" src="card/cross.svg" alt="remove-book">
+          <p class="book-title">${element.title}</p>
+          <p class="book-author">${element.author}</p>
+          <p class="pages-info">${element.pages}</p>
           <div class="icons">
-              <img src="card/heart.svg" alt="" class="card-icon">
-              <img src="card/check-square.svg" alt="" class="card-icon">
+              <img src="card/heart.svg" alt="" class="card-icon" id ="">
+              <img src="card/check-square.svg" alt="" class="card-icon" id="read-btn">
           </div>
-      </div>
-
-  `;
+      </div>`;
         newCard.innerHTML = cardHtml;
         newCard.classList.add('card');
         mainArea.appendChild(newCard);
+        if (element.isRead) {
+            newCard.querySelector('#read-btn').classList.add('active');
+        }
     });
 }
 
+//Function to remove book from the library (to be used on the cross button)
+//Function removes the Card element opacity and then removes it from the dom and removes the corresponding object from the myLibrary
 
 
 
@@ -56,7 +81,7 @@ document.getElementById('toggle-btn').addEventListener('click', () => {
     document.documentElement.classList.toggle('invert');
     document.querySelector('.header').classList.toggle('no-shadow');
     document.querySelector('.sidebar').classList.toggle('no-shadow');
-
+    document.querySelectorAll('.book-cover').forEach(element => (element.classList.toggle('invert')));
 })
 
 
